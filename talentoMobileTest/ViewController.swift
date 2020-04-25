9a69da156832ed83fa2af5a5647e87ee6fcbd043
Jose Santiago <josesantiluna@gmail.com>
2020-04-25 13:09:12 -0500

    Initial Commit

---
+++ talentoMobileTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  talentoMobileTest
+//
+//  Created by  Jose  Santiago on 4/25/20.
+//  Copyright © 2020  Jose  Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
